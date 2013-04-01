6cff2abae4964ce8e3e783b84065dde60607a36b
Adam McNicol <adamsorders@mac.com>
2013-04-01 09:54:29 +0100

    browse data now refreshes table view when switching from another tab
    Signed-off-by: Adam McNicol <adamsorders@mac.com>

--- sqlite_browse_data.py
+++ sqlite_browse_data.py
@@ -21,12 +21,14 @@ class BrowseDataWidget(QWidget):
         self.available_tables.currentIndexChanged.connect(self.change_table)
 
     def update_layout(self,db):
-        if db != None:
+        if db != None and self.db == None:
             self.db = db
             self.available_tables.clear()
             self.available_tables.addItems(self.db.tables())
             self.model = QSqlRelationalTableModel()
             self.change_table(0)
+        else:
+            self.model.select() #refresh the view
 
         
 
