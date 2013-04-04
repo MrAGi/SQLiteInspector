cb35544947d69f77cf45ab9e84ef3b3f676f1457
Adam McNicol <adamsorders@mac.com>
2013-04-04 15:55:56 +0100

    added licence information
    Signed-off-by: Adam McNicol <adamsorders@mac.com>

--- sqlite_connection.py
+++ sqlite_connection.py
@@ -11,6 +11,21 @@ Interface:
 Functional Requirements:
      Provides a class that can be instantiated to manage connections to a SQLite3 databases.
     User must provide a valid path to a SQLite3 database
+Licence:
+    Copyright (C) 2013  Adam McNicol
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
 """
 
 from PyQt4.QtSql import *
@@ -92,7 +107,7 @@ class SQLConnection:
         Takes one argument:
             sql - the sql query to be executed
         """
-        
+
         if not self.db.isOpen():
             self.open_database()
 
--- sqlite_entity_descriptions.py
+++ sqlite_entity_descriptions.py
@@ -11,6 +11,21 @@ Interface:
 Functional Requirements:
      Provides a widget that displays entity descriptions from a SQLite3 databases.
     User must provide a valid SQLite3 database connection to this widget
+Licence:
+    Copyright (C) 2013  Adam McNicol
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
 """
 
 from PyQt4.QtGui import *
